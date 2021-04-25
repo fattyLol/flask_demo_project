d62863ddf5efbdede1c95608aeb7fc6a73e1039e
19130282 <PATelepaev@sberbank.ru>
2021-04-25 23:16:23 +0300

    added db and save of an article

--- blog.py
+++ blog.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(127), nullable=False)
+    intro = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
@@ -9,18 +25,26 @@ def index():
 
 
 @app.route('/register.html')
-def try_exit():
+def register():
     return render_template('register.html')
 
 
-@app.route('/user/<string:name>/<int:user_id>')
-def hello_user(name, user_id):
-    return 'Hello ' + name + ' your id is ' + str(user_id)
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
 
-
-@app.route('/template-test')
-def template_tets():
-    return render_template('test.html')
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error occurred while saving the article"
+    else:
+        return render_template('create-article.html')
 
 
 if __name__ == '__main__':
